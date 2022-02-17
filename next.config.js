0751fecf5d4ff54085cf06a2479c2e064abc8382
matheusoliv23 <matheushenri1996@gmail.com>
2022-02-17 13:31:05 -0300

    Começo do projeto

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// @ts-check
 
-module.exports = nextConfig
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
+  /* config options here */
+};
+
+module.exports = nextConfig;
