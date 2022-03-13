383a681b80de97199c6e713d02fa4205a075d2be
matheusoliv23 <matheushenri1996@gmail.com>
2022-03-13 07:50:24 -0300

    Deploy para acompanhar o andamento do projeto

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,7 @@
-import { AppProps } from "next/dist/shared/lib/router/router";
+import type { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from "../styles/global";
 import theme from "../styles/theme";
-
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
