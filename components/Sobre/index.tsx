a535e2aca3a847915eb59937615f43a877d1d4cd
matheusoliv23 <matheushenri1996@gmail.com>
2022-04-10 10:54:03 -0300

    Container sobre

--- components/Sobre/index.tsx
+++ components/Sobre/index.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from "react";
 
-import { Container } from "./styles";
+import { Card, Container, ContainerCards, Subtitle } from "./styles";
 
 interface SobreProps {
   children: ReactNode;
@@ -9,9 +9,35 @@ interface SobreProps {
 export default function SobreNos() {
   return (
     <Container>
-      <h1>Onde estamos</h1>
-      <p>Estamos situados no Shopping de Sete Lagoas em Minas Gerais</p>
-      <p>Avenida Otávio Campelo Ribeiro nº 2801, Eldorado</p>
+      <div>
+        <h1>Onde estamos</h1>
+        <p>Estamos situados no Shopping de Sete Lagoas em Minas Gerais</p>
+        <p>Avenida Otávio Campelo Ribeiro nº 2801, Eldorado</p>
+      </div>
+
+      <ContainerCards>
+        <Card>
+          <Subtitle>Missão</Subtitle>
+          <div>
+            Ser a empresa de maior credibilidade do mercado de movimentação de
+            materiais, oferecendo as melhores soluções aos nossos clientes.
+          </div>
+        </Card>
+        <Card>
+          <Subtitle>Visão</Subtitle>
+          <div>
+            Ser a empresa de maior credibilidade do mercado de movimentação de
+            materiais, oferecendo as melhores soluções aos nossos clientes.
+          </div>
+        </Card>
+        <Card>
+          <Subtitle>Valores</Subtitle>
+          <div>
+            Ser a empresa de maior credibilidade do mercado de movimentação de
+            materiais, oferecendo as melhores soluções aos nossos clientes.
+          </div>
+        </Card>
+      </ContainerCards>
     </Container>
   );
 }
--- components/Sobre/styles.ts
+++ components/Sobre/styles.ts
@@ -2,4 +2,37 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  gap: 4rem;
+`;
+
+export const ContainerCards = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  align-items: center;
+  gap: 2rem;
+
+  @media screen and (max-width: 900px) {
+    grid-template-columns: 1fr;
+  }
+`;
+
+export const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background: rgba(164, 164, 164, 0.1);
+  border: 1px solid #cccccc;
+  height: 17rem;
+  padding: 3.625rem 2rem;
+  padding-right: 3rem;
+  padding-left: 3rem;
+  border-radius: 1.5rem;
+  text-align: center;
+`;
+
+export const Subtitle = styled.h3`
+  font-size: 1.5rem;
 `;
