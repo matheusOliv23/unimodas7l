76cd1d082babc823b11f63d95ab1e1b6a348192b
matheusoliv23 <matheushenri1996@gmail.com>
2022-06-14 20:55:14 -0300

    Removendo texto do whats

--- components/Whats/index.tsx
+++ components/Whats/index.tsx
@@ -5,9 +5,6 @@ import { Container, ContainerWidget } from "./style";
 export default function index() {
   return (
     <ContainerWidget>
-      <span>
-        Atendimento <br /> Whats
-      </span>
       <Container>
         <a
           href="https://api.whatsapp.com/send/?phone=5531996662795&text&app_absent=0"
